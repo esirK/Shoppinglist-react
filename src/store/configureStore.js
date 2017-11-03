1b1d3339ca319352af68ed47f4d02803d4d2b2d2
einsteincarrey <forensicslabnet@gmail.com>
2017-11-03 12:18:02 +0300

    Add thunk middleware to store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'; // TODO: REMOVE THIS MIDDLEWARE
 import { composeWithDevTools } from 'redux-devtools-extension'; // TODO: REMOVE THIS DEBUG TOOL
+import thunk from 'redux-thunk';
 
 const composeEnhancers = composeWithDevTools({}); // TODO: REMOVE THIS DEBUG TOOL
 
@@ -9,6 +10,6 @@ export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(reduxImmutableStateInvariant())), // TODO: REMOVE THIS MIDDLEWARE
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())), // TODO: REMOVE THIS MIDDLEWARE
     );
 }
