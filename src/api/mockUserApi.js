b3a7868337ffb859452aa2af9564d56c357dc7fa
einsteincarrey <forensicslabnet@gmail.com>
2017-11-04 12:37:26 +0300

    Create a mock api for user signup

---
+++ src/api/mockUserApi.js
@@ -0,0 +1,65 @@
+import {delay}  from './constants'; // setTimeout delay to simulate the delay of an AJAX call to a server.
+
+// This file mocks the shoppinglist web API
+const users = [
+    {
+        id: 123456,
+        username: "einstein",
+        firstname: "einstein",
+        lastname: "carrey",
+        password_hash: "d033e22ae348aeb5660fc2140aec35850c4da997",
+        answer: "einstein",
+        security_question: "Who am i?"
+    },
+    {
+        id: 4567,
+        username: "james",
+        firstname: "james",
+        lastname: "bond",
+        password_hash: "1d2bba5d0b80938327ac901264bcf7d4fe492fe9",
+        answer: "act movies",
+        security_question: "What do i do?"
+    }
+];
+
+//Generate a random user ID
+const generateId = () => {
+    return Math.random();
+};
+
+class UserApi {
+
+    static createUser(user) {
+        user = Object.assign({}, user); // create a copy of object passed in to avoid manipulating object passed in.
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                // Simulate server-side form data validation
+                if ((user.username == "") || (user.password == "")) {
+                    reject("Please provide a valid username and password");
+                }
+                if ((user.security_question == "") || (user.answer == "")) {
+                    reject("Please provide a valid security question and answer");
+                }
+                if (user.password.length < 6) {
+                    reject("password must be at-least 6 characters long");
+                }
+
+                if((users.findIndex(a => a.username == user.username)) != -1){
+                    reject(`username \`${user.username}\` is already registered. Please provide a unique username`);
+                }
+
+
+                //Simulating creating a user account
+                user.id = generateId();
+                users.push(user);
+
+
+                user.message = `user \`${user.username}\` has been created`;
+                resolve(user);
+
+            }, delay);
+        });
+    }
+}
+
+export default UserApi;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,12 +4,12 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'; // T
 import { composeWithDevTools } from 'redux-devtools-extension'; // TODO: REMOVE THIS DEBUG TOOL
 import thunk from 'redux-thunk';
 
-const composeEnhancers = composeWithDevTools({}); // TODO: REMOVE THIS DEBUG TOOL
+const composeEnhancers = composeWithDevTools(); // TODO: REMOVE THIS DEBUG TOOL
 
 export default function configureStore(initialState) {
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())), // TODO: REMOVE THIS MIDDLEWARE
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())) // TODO: REMOVE THIS MIDDLEWARE
     );
 }
