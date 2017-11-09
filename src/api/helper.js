3f864e8dcf7751fb43723a5423748e8e11187f28
einsteincarrey <forensicslabnet@gmail.com>
2017-11-09 22:50:34 +0300

    Create helper file for shared functions and constants

---
+++ src/api/helper.js
@@ -0,0 +1,7 @@
+export const delay = 1000;
+
+//Generate a random integer
+export function generateRandomInt(){
+    const randomFloat = Math.random();
+    return Math.round(randomFloat * 1000000);
+};
