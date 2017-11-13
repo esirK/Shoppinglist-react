35346d14495587fa971f4237d53439093326980c
einsteincarrey <forensicslabnet@gmail.com>
2017-11-13 12:21:00 +0300

    Add links for specific lists rows

--- src/components/lists/ListsTableRow.js
+++ src/components/lists/ListsTableRow.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 const ListsTableRow = ({list}) => {
     return (
         <tr>
             <td>NO.</td>
-            <td>{list.title}</td>
+            <td>
+                <Link to={"/lists/"+ list.id}> {list.title} </Link>
+            </td>
             <td>{list.created_on}</td>
             <td>{list.updated_on}</td>
             <td>Actions</td>
