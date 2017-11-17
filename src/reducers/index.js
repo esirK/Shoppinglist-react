d41938e76e0afa92f785c150ad66129acd236980
Einstein <forensicslabnet@gmail.com>
2017-11-17 15:57:40 +0300

    Create errorMessage state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux';
 import user from './usersReducer';
 import lists from './listsReducer';
+import errorMessage from './errorReducer';
 
 const rootReducer = combineReducers({
     user,
-    lists
+    lists,
+    errorMessage
 });
 
 export default rootReducer;
