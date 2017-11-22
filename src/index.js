49149ee90d6b90b3a3f3e65c7cdfc5b53ae027fa
Einstein <forensicslabnet@gmail.com>
2017-11-22 23:36:32 +0300

    Prep for production builds

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+// eslint-disable-next-line import/default
 import configureStore from './store/configureStore';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
