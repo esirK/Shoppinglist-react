a106756ab21af280800d1b837244b6d504f1a184
Einstein <forensicslabnet@gmail.com>
2017-11-27 14:34:08 +0300

    Redirect user if not logged in

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import {Route, IndexRoute, browserHistory} from 'react-router';
 import App from './components/App';
 import Lists from './components/lists/Lists';
 import Items from './components/items/Items';
 import AuthenticationPage from './components/authentication/AuthenticationPage';
 
-const requireSignIn = () => {return;};
+
+function loggedIn() {
+   return localStorage.getItem("token") !== null;
+}
+
+function requireAuth(nextState, replace) {
+    if (!loggedIn()) {
+        replace({
+            pathname: '/login'
+        });
+    }
+}
 
 export default (
     <Route path="/" component={App}>
-            <IndexRoute component={requireSignIn(Lists)} />
+            <IndexRoute component={Lists}  onEnter={requireAuth} />
             <Route path="signup" component={AuthenticationPage} module="signup" />
             <Route path="login" component={AuthenticationPage} module="login" />
-            <Route path="items" component={requireSignIn(Items)} />
-            <Route path="lists" component={requireSignIn(Lists)} />
+            <Route path="items" component={Items} onEnter={requireAuth} />
+            <Route path="lists" component={Lists} onEnter={requireAuth} />
     </Route>
 );
